24b0b3b1c88efe86241fd3bef1e53274ed905006
Igor Henrique Siriani <igor.siriani@maua.br>
2025-01-23 16:57:16 -0300

    compilation

--- envs.d.ts
+++ envs.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
   interface ImportMetaEnv {
-    VITE_STAGE: string
-    VITE_API_URL: string
+    VITE_STAGE: string,
+    VITE_API_URL: string,
+    VITE_API_WITHDRAW: string
   }
 }
